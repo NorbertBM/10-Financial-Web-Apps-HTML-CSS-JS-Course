1676f86fdab38d89ab8750b13fde52814913a9fb
NorbertBM <menyhart.b.norbert@gmail.com>
2023-12-09 09:31:34 +0100

    ren in progress

---
+++ apps/07-savings-goal-tracker/src/modules/main.js
@@ -0,0 +1,21 @@
+// console.log("module");
+
+import { domElements } from "./dom-elements.js";
+
+const {
+  checkInterestRate,
+  interestRateInput,
+  goalAmount,
+  currentSavings,
+  monthlyContribution,
+  interestRateLabel,
+} = domElements();
+
+console.log(
+  checkInterestRate,
+  interestRateInput,
+  goalAmount,
+  currentSavings,
+  monthlyContribution,
+  interestRateLabel
+);
